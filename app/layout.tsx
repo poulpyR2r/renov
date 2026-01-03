c36fd5e6327fec1574945475519200f7358ef6da
poulpyR2r <guewencarre@gmail.com>
2026-01-03 16:10:54 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,19 +21,21 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/favicon-light.svg",
         media: "(prefers-color-scheme: light)",
+        type: "image/svg+xml",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/favicon-dark.svg",
         media: "(prefers-color-scheme: dark)",
+        type: "image/svg+xml",
       },
       {
         url: "/icon.svg",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/icon.svg",
   },
 };
 
